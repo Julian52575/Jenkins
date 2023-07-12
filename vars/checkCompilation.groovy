09bcd54ac3ecfe88431ba761dd8e346f9fa4db08
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2023-07-12 09:29:20 +0200

    Update checkCompilation.groovy

--- vars/checkCompilation.groovy
+++ vars/checkCompilation.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:] ) {
-    sh "echo "Hello world" "
+    sh 'echo "Hello world 1" '
+    sh "echo "Hello world 2" "
     sh "echo "Testing Compilation of ${config.name}" "
     sh 'ls'
     sh 'test Makefile'
