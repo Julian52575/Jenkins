d8fd00855ff31bbfb544513f9b19cfc03782e19d
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2023-07-12 09:24:33 +0200

    [fix] Changed ' to "

--- vars/checkCompilation.groovy
+++ vars/checkCompilation.groovy
@@ -1,9 +1,9 @@
 def call(Map config = [:] ) {
-    sh 'echo "Testing Compilation of ${config.name}" '
+    sh "echo "Testing Compilation of ${config.name}" "
     sh 'ls'
     sh 'test Makefile'
     sh 'make'
-    sh 'test -x ${config.name}'
-    sh 'echo ${config.name} has been correctly compiled !'
+    sh "test -x ${config.name}"
+    sh "echo ${config.name} has been correctly compiled !"
 }
 
