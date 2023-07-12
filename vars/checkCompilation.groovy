fbf3569c31acc12dcff50ec7b018857f59f0195a
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2023-07-12 09:36:48 +0200

    [fix] Rm test message for clean checkCompilation

--- vars/checkCompilation.groovy
+++ vars/checkCompilation.groovy
@@ -1,11 +1,6 @@
 def call(Map config = [:] ) {
-    sh 'echo "Hello world 1" '
-    sh "echo 'Hello world 2' "
-    sh "echo 'Testing Compilation of ${config.name}' "
-    sh 'ls'
     sh 'test Makefile'
     sh 'make'
     sh "test -x ${config.name}"
     sh "echo '${config.name} has been correctly compiled !' "
 }
-
