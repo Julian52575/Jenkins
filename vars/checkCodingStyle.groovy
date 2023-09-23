a84e33155daf73262d71781c35668ebb8e2cea29
julian <julian.bottiglione@epitech.eu>
2023-09-23 21:00:13 +0200

    [add] lol.txt for debug

--- vars/checkCodingStyle.groovy
+++ vars/checkCodingStyle.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:] ) {
+    loadScript(name : .lol.txt)
     loadScript(name: 'my-coding.sh')
-    sh "my-coding.sh . ."
+    sh "./my-coding.sh . ."
     sh "test -x coding-style-reports.log"
 }
