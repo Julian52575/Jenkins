477315c5aa9f283bc4463bcbe731255269ce14d3
julian <julian.bottiglione@epitech.eu>
2023-09-24 11:22:05 +0200

    [fix] passing whole path instead of script name in loadScript

--- vars/loadScript.groovy
+++ vars/loadScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-    def fileContent = libraryResource "scripts/${config.name}"
+    def fileContent = libraryResource "${config.name}"
     writeFile file: "${config.name}", text: fileContent
     sh "chmod a+x ./${config.name}"
 }
