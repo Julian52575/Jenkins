6cceeaa487aa2255605f059693fd6bd8f26e074a
julian <julian.bottiglione@epitech.eu>
2023-09-24 11:15:13 +0200

    [fix] remove quotation marks for script name

--- vars/checkCodingStyle.groovy
+++ vars/checkCodingStyle.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:] ) {
     loadScript(name: a.sh)
-    loadScript(name: "my-coding.sh")
+    loadScript(name: my-coding.sh)
     sh "./my-coding.sh . ."
     sh "test -x coding-style-reports.log"
 }
