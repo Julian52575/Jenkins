cdacca84004ba69910b14739fa0a52ed2bf022ca
julian <julian.bottiglione@epitech.eu>
2023-09-24 11:29:09 +0200

    [fix] changed loadScript to take a name of a script inside the scripts folder instead of name

--- vars/checkCodingStyle.groovy
+++ vars/checkCodingStyle.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:] ) {
-    loadScript(name: "./a.sh")
+    loadScript(name: "a.sh")
     sh "./a.sh"
-    loadScript(name: "scripts/my-coding.sh")
+    loadScript(name: "my-coding.sh")
     sh "./my-coding.sh . ."
     sh "test -x coding-style-reports.log"
 }
