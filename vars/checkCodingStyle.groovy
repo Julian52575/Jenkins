1bd94b239f3dd16e7a2ffc678b24d9b0072d130b
julian <julian.bottiglione@epitech.eu>
2023-09-24 11:17:57 +0200

    [fix] commentated loadScript(my-coding.sh)

--- vars/checkCodingStyle.groovy
+++ vars/checkCodingStyle.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [:] ) {
     loadScript(name: a.sh)
-    loadScript(name: my-coding.sh)
+    //loadScript(name: my-coding.sh)
+    sh "./a.sh"
     sh "./my-coding.sh . ."
     sh "test -x coding-style-reports.log"
 }
