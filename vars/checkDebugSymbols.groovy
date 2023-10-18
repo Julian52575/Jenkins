17145147ae0eaa40044165f12ed32960f03af62e
julian <julian.bottiglione@epitech.eu>
2023-10-18 20:19:32 +0200

    [fix] added apt-get file to Dockerfile && removed ls

--- vars/checkDebugSymbols.groovy
+++ vars/checkDebugSymbols.groovy
@@ -1,7 +1,6 @@
 def call(Map config = [:]) {
-    sh 'ls'
     hasDebugSymbol = sh (
-        script: "file ${config.name} | grep -q -v 'with debug_info'",
+        script: "file ${config.name} | grep -v 'with debug_info'",
         returnStatus: true
     )
     return hasDebugSymbol
