dd66d3016efb26c410ed4f5e840274810090e6e5
julian <julian.bottiglione@epitech.eu>
2023-10-18 20:41:14 +0200

    [fix] reset log before writing

--- vars/checkBasics.groovy
+++ vars/checkBasics.groovy
@@ -2,6 +2,7 @@ def call(Map config = [:]) {
     hasCompiled = 0
     hasDebugSymbols = 0
 
+    sh 'echo " " > new_mouli_log.txt'
     hasCompiled = checkCompilation( name:"math" )
     if ( hasCompiled == 1 ) {
         exit 84
