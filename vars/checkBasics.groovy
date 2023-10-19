6c3ae41df4f7eaee235f0a1ed6ebd825e9fde8ea
julian <julian.bottiglione@epitech.eu>
2023-10-19 22:57:39 +0200

    [fix] date hour syntax

--- vars/checkBasics.groovy
+++ vars/checkBasics.groovy
@@ -2,9 +2,11 @@ def call(Map config = [:]) {
     hasCompiled = 0
     date = "01 January 1900"
     hour = "0:01"
-    header = "\t\t${config.name}\n${date}\t\t${hour}\n\n"
 
-    sh "echo ${header} > new_mouli_log.txt"
+    sh "echo ${config.name} > new_mouli_log.txt"
+    sh "echo ${config.author} >> new_mouli_log"
+    sh "echo ${date} >> new_mouli_log.txt"
+    sh "echo ${hour} >> new_mouli_log.txt"
     hasCompiled = checkCompilation( name:"${config.name}" )
     if ( hasCompiled == 1 ) {
         exit 84
