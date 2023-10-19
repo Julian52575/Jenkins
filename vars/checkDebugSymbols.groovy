f84100270510af5ab6e8f1c000479e5afcb3a4b5
julian <julian.bottiglione@epitech.eu>
2023-10-19 21:58:57 +0200

    [fix] missing quotation marks "

--- vars/checkDebugSymbols.groovy
+++ vars/checkDebugSymbols.groovy
@@ -3,11 +3,11 @@ def call(Map config = [:]) {
         script: "file ${config.name} | grep -v 'with debug_info'",
         returnStatus: true
     )
-    sh 'echo -n "[[[Debug symbol:\t'
+    sh 'echo -n "[[[Debug symbol:\t" >> new_mouli_log.txt'
     if ( hasDebugSymbol == "0" ) {
-        sh 'echo "OK. No debbuging symbols present in binary."'    
+        sh 'echo "OK. No debbuging symbols present in binary." >> new_mouli_log.txt'    
     } else {
-        sh 'echo "KO. Binary has been compiled with debbuging info."'    
+        sh 'echo "KO. Binary has been compiled with debbuging info." >> new_mouli_log.txt'
     }
     return hasDebugSymbol
 }
