259221e1d965e01f9c3b70b5c2c2e378e3b21451
julian <julian.bottiglione@epitech.eu>
2023-10-19 23:20:29 +0200

    [add] fuck echo -e

--- vars/checkBasics.groovy
+++ vars/checkBasics.groovy
@@ -7,7 +7,12 @@ def call(Map config = [:]) {
     sh "echo ${config.author} >> new_mouli_log.txt"
     sh "echo ${date} >> new_mouli_log.txt"
     sh "echo ${hour} >> new_mouli_log.txt"
-    sh 'echo -e "\n\n---\n" >> new_mouli_log.txt'
+    sh 'echo " " >> new_mouli_log.txt'
+    sh 'echo " " >> new_mouli_log.txt'
+
+    sh 'echo "---" >> new_mouli_log.txt'
+    
+    sh 'echo " " >> new_mouli_log.txt'
     hasCompiled = checkCompilation( name:"${config.name}" )
     if ( hasCompiled == 1 ) {
         exit 84
