5aff9f20378bdec21ec5c9306610191525c9c9d3
julian <julian.bottiglione@epitech.eu>
2023-10-19 22:50:12 +0200

    [fix] Added header of New_mouli_log.txt

--- vars/checkBasics.groovy
+++ vars/checkBasics.groovy
@@ -1,12 +1,13 @@
 def call(Map config = [:]) {
     hasCompiled = 0
-    hasDebugSymbols = 0
+    date = "01 January 1900"
+    hour = "0:01"
 
-    sh 'echo " " > new_mouli_log.txt'
-    hasCompiled = checkCompilation( name:"math" )
+    sh "echo \t\t${config.name}\n${date}\t\t${hour}\n\n > new_mouli_log.txt"
+    hasCompiled = checkCompilation( name:"${config.name}" )
     if ( hasCompiled == 1 ) {
         exit 84
     }
     //checkMakefileClean( name:"math" )
-    hasDebugSymbols = checkDebugSymbols( name:"math" )
+    hasDebugSymbols = checkDebugSymbols( name:"${config.name}" )
 }
