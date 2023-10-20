b48289dbc53d2678fa09e688e9598740cd3e62ad
julian <julian.bottiglione@epitech.eu>
2023-10-20 15:58:44 +0200

    [add] prettier log

--- vars/runTest.groovy
+++ vars/runTest.groovy
@@ -8,7 +8,7 @@ def call(Map config = [:]) {
     sh 'echo "Hello from runTest" '
     //Starting log
     sh "echo -n ${config.name}: >> new_mouli_log.txt"
-    sh 'echo -n "\t|\t" >> new_mouli_log.txt'
+    sh 'echo -n "\t\t|\t" >> new_mouli_log.txt'
     //
     //Stdout output
     output = sh (
@@ -34,7 +34,7 @@ def call(Map config = [:]) {
     } else {
         printKO()
     }
-    sh 'echo -n "\t\t" >> new_mouli_log.txt'
+    sh 'echo -n "\t|\t" >> new_mouli_log.txt'
     //
     //newline for next test
     sh "echo ${config.cmd}:\t >> new_mouli_log.txt"
