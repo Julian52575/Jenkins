73fe8fcb5261b15e5c7da2d38d4d76c6a245f4ca
julian <julian.bottiglione@epitech.eu>
2023-10-20 14:21:56 +0200

    [fix] echoed flaged instead of applying them

--- vars/printOK.groovy
+++ vars/printOK.groovy
@@ -1,7 +1,7 @@
 def call() {
-    GRN="\033[0;32m"
-    NC="\033[0m"
+    GRN=\033[0;32m
+    NC=\033[0m
 
-    sh "echo -e -n ${GRN}OK${NC}.  >> new_mouli_log.txt"
+    sh 'echo -e -n "${GRN}OK${NC}. " >> new_mouli_log.txt'
 }
 
