64497c249e726773156ac47c6e5af1887dc77afa
julian <julian.bottiglione@epitech.eu>
2023-10-20 16:11:10 +0200

    [add] prettier log

--- vars/printTable.groovy
+++ vars/printTable.groovy
@@ -1,5 +1,5 @@
 def call() {
-    sh 'echo "--------------------------------------------------------------------------------------------" >> new_mouli_log.txt'
+    sh 'echo "-----------------------------------------------------------------------------------------" >> new_mouli_log.txt'
     sh 'echo "Test Name\t|\tStdOutput\t|\tReturnValue\t|\tCommand\t\t|" >> new_mouli_log.txt'
 }
 
