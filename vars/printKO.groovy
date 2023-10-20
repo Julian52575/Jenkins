8942485ffe911f54cb06f7b85dbc572aba1991fe
julian <julian.bottiglione@epitech.eu>
2023-10-20 14:42:18 +0200

    [fix] Removed Colors and echo -e due to Jenkins non support

--- vars/printKO.groovy
+++ vars/printKO.groovy
@@ -1,7 +1,4 @@
 def call() {
-    RED="\033[0;31m"
-    NC="\033[0m"
-
-    sh 'echo -e -n "${RED}KO${NC}. " >> new_mouli_log.txt'
+    sh 'echo -n "KO. " >> new_mouli_log.txt'
 }
 
--- vars/printOK.groovy
+++ vars/printOK.groovy
@@ -1,6 +1,4 @@
 def call() {
-    sh (
-        script: 'echo -n "OK. " >> new_mouli_log.txt'
-    )
+    sh 'echo -n "OK. " >> new_mouli_log.txt'
 }
 
