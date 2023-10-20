84f8e66a0f8642c74e10b821ddd7a3efa8a724c1
julian <julian.bottiglione@epitech.eu>
2023-10-20 14:37:47 +0200

    [fix] rm -e unsuported by Jenkins

--- vars/printOK.groovy
+++ vars/printOK.groovy
@@ -1,7 +1,7 @@
 def call() {
     message = '\033[0;32mOK\033[0m. '
     sh (
-        script: 'echo -en "${message}" >> new_mouli_log.txt'
+        script: 'echo -n "${message}" >> new_mouli_log.txt'
     )
 }
 
