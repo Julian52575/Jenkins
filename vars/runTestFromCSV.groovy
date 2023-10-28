6b2b4830fbfaab78bc338b0040160a3491537164
julian <julian.bottiglione@epitech.eu>
2023-10-28 23:26:08 +0200

    [add] runTestFromCSV

---
+++ vars/runTestFromCSV.groovy
@@ -0,0 +1,25 @@
+def call(Map config = [:]) {
+    //name -> 1+1
+    //cmd -> ./math 1 1 +
+    //expOutput -> Your result is 2.
+    //expReturnValue -> 0
+    def csvFilePath = "./JenkinsNewMouli.csv"
+    if ( fileExists(csvFilePath) == false ) {
+        return 84
+    }
+
+    def csvFile = readFile csvFilePath
+    
+    def csvLines = csvFile.readLines()
+
+    for (def line in csvLines) {
+        def fields = line.split(',')
+
+        runTest(
+            name: fields[0],
+            cmd: fields[1],
+            expOutput: fields[2],
+            expReturnValue: fields[3]
+        )
+    }
+}
