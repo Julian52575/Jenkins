4d718afdbe3fb780662a41bcd97609f3ef322ce3
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-01 18:10:44 +0100

    Split printHeader and checkBascics

--- lib/vars/checkBasics.groovy
+++ lib/vars/checkBasics.groovy
@@ -1,16 +1,6 @@
 def call(Map config = [:]) {
     hasCompiled = 0
-    author = currentBuild.getBuildCauses()[0].shortDescription + " / " + currentBuild.getBuildCauses()[0].userId
-    
-    //Prints Header
-    sh "echo ${config.name} > ${config.logName}"
-    //sh "echo ${config.author} >> ${config.logName}"
-    sh "echo ${author} >> ${config.logName}"
-    sh "date '+%A %d %B - %H:%M' >> ${config.logName}"
-    sh "echo '---' >> ${config.logName}"
-    sh "echo ' ' >> ${config.logName}"
-    sh "echo ' ' >> ${config.logName}"
-    //
+
     hasCompiled = checkCompilation( 
                     name:"${config.name}",
                     logName: "${config.logName}",
