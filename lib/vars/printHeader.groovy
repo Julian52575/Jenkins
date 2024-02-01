34ac5c0847d5776e970b2bee3db11eca09c1011d
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-01 18:10:13 +0100

    Create printHeader.groovy

---
+++ lib/vars/printHeader.groovy
@@ -0,0 +1,12 @@
+def call(Map config = [:]) {
+    hasCompiled = 0
+    author = currentBuild.getBuildCauses()[0].shortDescription + " / " + currentBuild.getBuildCauses()[0].userId
+    date = date '+%A %d %B - %H:%M'
+    merge = config.name + '|' + author + '|' + date + '|' 
+    strlen = ${#merge}
+
+    //Prints Header
+    sh "echo '${config.name}|${author}|${date}|' > ${config.logName}"
+    sh "printf '%0.s-' {1..${strlen}} >> ${config.logName}"
+    sh "echo ' ' >> ${config.logName}"
+}
