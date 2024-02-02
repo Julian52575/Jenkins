39a0cd64fa6e1eda726f48a5065c34b8250e4fe0
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-02 09:51:54 +0100

    Create runAndLogCommand.groovy

---
+++ lib/vars/runAndLogCommand.groovy
@@ -0,0 +1,17 @@
+// config.cmd -> command to run
+// config.logName -> logFile to write result into (if any)
+def call(Map config = [:]) {
+    hasCompiled = 0
+    stdOutput = ""
+  
+    stdOutput = sh (
+        script: ' ${config.cmd},
+        returnStdout: true 
+    ) == 0
+    if ( ${config.logname} == "" ) {
+      sh "echo 'Result: _${stdOutput}_' "
+    } else {
+      sh "echo 'Ok _${stdOutput}_' >> ${config.logName}"
+    }
+    return 0
+}
