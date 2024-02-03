5095cb4195247d107b23586d63136584a4d2413b
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-04 00:15:32 +0100

    Add bash script support in CSV

--- lib/vars/runTestFromCSV.groovy
+++ lib/vars/runTestFromCSV.groovy
@@ -5,20 +5,30 @@
     //expReturnValue -> 0
     //logName -> New_mouli.log
     //depthName -> InDepth.log
-    //CSVname -> ./NMtests.csv
+    //CSVpath -> ./NMtests.csv
 
 def call(Map config = [:]) {
-    if ( fileExists("${config.CSVname}") == false ) {
-        sh "echo 'Cannot find CSV name for testing\"${config.CSVname}\".' >> ${config.logName}"
-        return 0
-    }
-    def csvFile = readFile "${config.CSVname}"
+    def csvPath = config.CSVpath
     
-    def csvLines = csvFile.readLines()
+    if ( fileExists("${config.CSVpath}") == false ) {
+        sh "echo No CSV file for testing called _${config.CSVpath}_"
+        return false
+    }
+    def csvContent = readFile "${config.CSVname}"
+    def csvLines = csvContent.readLines()
 
     for (def line in csvLines) {
         def fields = line.split(',')
-
+        def commandToRun = fields[1]
+        
+        if (fields[1] != "" && fields[1][0] == '.' && fields[1][1] == '/') {//if cmd is a path to a script
+            loadScript(
+                name: "tmp.sh",
+                path: field[1]
+            )
+            commandToRun = "bash tmp.sh"
+        }
+        
         if ( fields[0] != "name" && fields[1] != "cmd") { //skip csv header
             runTest(
                 name: fields[0],
@@ -30,4 +40,5 @@ def call(Map config = [:]) {
             )
         }
     }
+    return true
 }
