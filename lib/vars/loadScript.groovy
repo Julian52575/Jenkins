02e8b5719fe04c245cca4b86dcff036efd2638fb
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-04 00:05:18 +0100

    Added filePath to loadScript.groovy

--- lib/vars/loadScript.groovy
+++ lib/vars/loadScript.groovy
@@ -1,5 +1,22 @@
+
+    // config.name = new script name
+    // config.path = original script path
+
+//bool
 def call(Map config = [:]) {
-    def fileContent = libraryResource "./scripts/${config.name}"
-    writeFile file: "${config.name}", text: fileContent
-    sh "chmod a+x ./${config.name}"
+    def fileContent = ""
+    def fileName = config.name
+    def filePath = config.path
+
+    if ( config.name == "" ) {
+        sh "echo "Library Warning:\t loadScript called without proper config.name, skipping..."
+        return false
+    }
+    if ( config.path == "" )
+        filePath = fileName
+    
+    fileContent = libraryResource filePath
+    writeFile file: fileName, text: fileContent
+    sh "chmod a+x ${filePath}"
+    return true
 }
