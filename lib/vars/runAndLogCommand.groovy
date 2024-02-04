5517a247020a1ff93d062560827e29030bb4a37a
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-04 13:36:00 +0100

    Update runAndLogCommand.groovy

--- lib/vars/runAndLogCommand.groovy
+++ lib/vars/runAndLogCommand.groovy
@@ -1,26 +1,33 @@
 // config.cmd -> command to run
-// config.logName -> logFile to write result into (if any)
-// config.errorFile -> file to log errors into
+// config.expOutput -> expected output to stdin
+// config.expStatus -> expected return status
+// config.logPath -> logFile to write result into (if any)
+// config.errorPath -> file to log errors into
+
 def call(Map config = [:]) {
     def hasCompiled = 0
-    def classResult
     def stdOutput = ""
     def status = 0
+    def expOutput = config.expOutput
+    def expStatus = config.expStatus
+    def classResult
+    def logPath = config.logPath
+    def errorPath = config.errorPath
 
+    //Run command and store in classResult
     //https://stackoverflow.com/questions/159148/groovy-executing-shell-commands
-    sh "echo Executing ${config.cmd}"//////
-    
+    sh "echo Executing ${config.cmd}..."//////
     classResult  = "${config.cmd}".execute()
-    stdOutput = classResult.text()
+    stdOutput = classResult.text
     //status = classResult.status()
-
+    
     if ( status != 0 )
         return status
     stdOutput = readFile('tmp.txt').trim()
     if ( ${config.logname} == "" ) {
       sh "echo 'Result: _${stdOutput}_' "
     } else {
-      sh "echo 'Ok _${stdOutput}_' >> ${config.logName}"
+      sh "echo 'Ok _${stdOutput}_' >> ${logPath}"
     }
     return 0
 }
