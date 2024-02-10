409e77d9c10146187d3b67edd74c70b5b6972cf4
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-10 21:38:58 +0100

    Update runSetupFromCSV.groovy

--- lib/vars/runSetupFromCSV.groovy
+++ lib/vars/runSetupFromCSV.groovy
@@ -11,6 +11,8 @@ def call(Map config = [:]) {
     for (def line in csvLines) {
         def fields = line.split(',')
 
+        if (fields.empty() == true)
+            continue
         if ( fields[0] != "cmd" ) { //skip CSV header
             sh (
                 script: "${fields[0]}",
