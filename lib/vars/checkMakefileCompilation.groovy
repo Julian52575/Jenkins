9164ae5cd6a4c8dcbaf4d9c4d7f5e06f22e1544b
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-10 23:30:36 +0100

    Update checkMakefileCompilation.groovy

--- lib/vars/checkMakefileCompilation.groovy
+++ lib/vars/checkMakefileCompilation.groovy
@@ -1,15 +1,14 @@
 def call(Map config = [:] ) {
-    def boolean hasCompiled = false
-    def boolean hasMakefile = false
-    def boolean isExecutable = false
-    def String boolStrBuffer = "0"
-    def String compilationLog = ""
-    def String logPath = config.logPath
-    if (logPath == null)
-        logPath = "Result.log"
     def String binaryName = config.binaryName
     if (binaryName == null)
         return false
+    def String logPath = config.logPath
+    if (logPath == null)
+        logPath = "Result.log"
+    def boolean hasCompiled = false
+    def boolean hasMakefile = false
+    def boolean isExecutable = false
+    def String compilationLog = ""
     
     sh "echo -n '*Compilation: \t' >> ${logPath}"
     //TEST MAKEFILE
@@ -18,9 +17,6 @@ def call(Map config = [:] ) {
         returnStatus: true
     ) == 0
     if ( hasMakefile == false ) {
-        printKO(
-            logName: logPath
-        )
         sh "echo 'KO. No Makefile are present in the workspace.' >> ${logPath}"
         return false
     }
@@ -38,9 +34,6 @@ def call(Map config = [:] ) {
         return false
     }
     if ( hasCompiled == false ) {
-        printKO(
-            logName: logPath
-        )
         sh "echo 'KO. Compilation failed with status non 0.' >> ${logPath}"
         return false
     }
