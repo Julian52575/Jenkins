ffda44c736c6212934d5a193aaeaed5e40c597cd
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-10 18:33:35 +0100

    Create executeCmdFromString.groovy

---
+++ lib/vars/executeCmdFromString.groovy
@@ -0,0 +1,24 @@
+//    config.cmd    ->    command to run as a string
+@NonCPS
+def executeCmdFromString(Map config = [:]) {
+    def String stdOutput = ""
+    def int status = 0
+    def process = null
+
+    if ( config.cmd == null )
+        return ["", 0]
+    try {
+        //Run command thanks to java.lang.Process (f*ck the documentation tho)
+        process = config.cmd.execute()
+        if ( process.isAlive() ) {
+            process.waitFor()
+        }
+        status = process.exitValue()
+        stdOutput = process.getText().trim()
+        //process.destroy()
+    } catch (Exception e) {
+        throw e
+    }
+    return [stdOutput, status]
+}
+//returns a list made of stdOutput and status code
