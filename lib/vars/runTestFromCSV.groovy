21d6c580317f6e41e9005eef8c4c473c7ceefbc4
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-10 21:37:24 +0100

    Update runTestFromCSV.groovy

--- lib/vars/runTestFromCSV.groovy
+++ lib/vars/runTestFromCSV.groovy
@@ -24,6 +24,9 @@ def call(Map config = [:]) {
     for (def line in csvLines) {
         def fields = line.split(',')
         def commandToRun = fields[1]
+
+        if (fields.length() < 4)
+            continue
         
         if (fields[1] != "" && fields[1][0] == '.' && fields[1][1] == '/') {//if cmd is a path to a script
             loadScript(
