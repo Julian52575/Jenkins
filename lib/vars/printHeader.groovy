5121e1b1a0e914972d6b4f6abb5740a101d0941e
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-10 21:56:29 +0100

    Update printHeader.groovy

--- lib/vars/printHeader.groovy
+++ lib/vars/printHeader.groovy
@@ -2,19 +2,19 @@ def call(Map config = [:]) {
     def int hasCompiled = 0
     def String mergedText = ""
     def String chinaWall = ""
-    //def String strlen = ""
+    def String strlen = ""
     def String author = currentBuild.getBuildCauses()[0].shortDescription + " / " + currentBuild.getBuildCauses()[0].userId
     def String currentTime = sh (
         script: "date '+%A %d %B - %H:%M' | tr -d '\n'",
         returnStdout: true
     )
     mergedText = config.name + ' | ' + author + ' | ' + currentTime + ' | '
-    //strlen = sh (
-    //    script: 'echo -n "${mergedText}" | wc -c | tr -d "\n" ',
-    //    returnStdout: true
-    //)
+    strlen = sh (
+        script: 'echo -n "${mergedText}" | wc -c | tr -d "\n" ',
+        returnStdout: true
+    ).trim()
     chinaWall = sh (
-                    script: " printf %100s |tr ' ' '=' ",
+                    script: " printf %${strlen}s |tr ' ' '-' ",
                     returnStdout: true
                 )
 
