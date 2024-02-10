1c62db8e973b57f9f8cb4a8018d40c589237ddc7
Julian Bottiglione <114570075+Julian52575@users.noreply.github.com>
2024-02-10 18:33:41 +0100

    Update runAndLogCommand.groovy

--- lib/vars/runAndLogCommand.groovy
+++ lib/vars/runAndLogCommand.groovy
@@ -61,30 +61,6 @@ def logOK(Map config = [:]) {
     )
 }
 
-//    config.cmd    ->    command to run as a string
-@NonCPS
-def executeCmdFromString(Map config = [:]) {
-    def String stdOutput = ""
-    def int status = 0
-    def process = null
-
-    if ( config.cmd == null )
-        return ["", 0]
-    try {
-        //Run command thanks to java.lang.Process (f*ck the documentation tho)
-        process = config.cmd.execute()
-        if ( process.isAlive() ) {
-            process.waitFor()
-        }
-        status = process.exitValue()
-        stdOutput = process.getText().trim()
-        //process.destroy()
-    } catch (Exception e) {
-        throw e
-    }
-    return [stdOutput, status]
-}
-
 def call(Map config = [:]) {
     def String commandToRun = config.cmd
     if ( commandToRun == null )
